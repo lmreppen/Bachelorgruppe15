864481807df3dc5c154a18fb3d8e816cdb3f65d6
Kenneth <kenneth.solstad@gmail.com>
2016-04-13 14:33:45 +0200

    Fiks i javascript

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 # Redirect http to https
-var host = "lmreppen.github.io/Bachelorgrupp15";
+var host = "lmreppen.github.io/Bachelorgruppe15/";
 if ((host == window.location.host) && (window.location.protocol != "https:"))
 window.location.protocol = "https";

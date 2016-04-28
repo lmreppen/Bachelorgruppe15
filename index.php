f5f266a3af1a28f495bff722ce4f41053b7b3f4d
ssbolsta <syver92@msn.com>
2016-04-28 12:59:34 +0200

    Prøver å legge til notification knapp

--- index.php
+++ index.php
@@ -97,7 +97,47 @@
 
         <div class="container-fluid" id="timetable">
             <button type="button" id="stoppKnapp1" class="btn btn-xs btn-default btn-danger" >STOPP</button>
+            <button onclick="notifyMe()"> Notify me!</button>
+<script type="text/javascript">
+  function notifyMe() {
+  // Let's check if the browser supports notifications
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  }
+
+  // Let's check whether notification permissions have already been granted
+  else if (Notification.permission === "granted") {
+    // If it's okay let's create a notification
+    var notification = new Notification("Hi there!");
+  }
+
+  // Otherwise, we need to ask the user for permission
+  else if (Notification.permission !== 'denied') {
+    Notification.requestPermission(function (permission) {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        var notification = new Notification("Hi there!");
+      }
+    });
+  }
+
+
+  // At last, if the user has denied notifications, and you 
+  // want to be respectful there is no need to bother them any more.
+}
+
+</script>
             
+
+        
+
+  // At last, if the user has denied notifications, and you 
+  // want to be respectful there is no need to bother them any more.
+}
+
+</script>
+
+
           <div class="simple-template-container">
             <!-- dynamically generated content from template is placed here -->
 

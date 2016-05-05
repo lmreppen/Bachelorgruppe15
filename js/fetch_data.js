7456e344ca1e930bcdac43d777fe4e0efac9af67
lmreppen <lmreppen@stud.ntnu.no>
2016-05-05 10:09:44 +0200

    adding stop.js, moving code from fetch_data.js

--- js/fetch_data.js
+++ js/fetch_data.js
@@ -157,38 +157,7 @@ function generateFromTemplate(json){
 
 
 }
-const serviceUUID = '00001523-1212-efde-1523-785feabcd123';
-const ledCharacteristicUUID = '00001525-1212-efde-1523-785feabcd123';
-
-var bleDevice;
-var bleServer;
-var bleService;
 
 
-window.onload = function() {
-    document.querySelector('#stoppKnapp1').addEventListener('click', connect);
-    document.querySelector('#stoppKnapp').addEventListener('click', connect);
-}
-function connect() {
-  if (!navigator.bluetooth) {
-
-      return;
-  }
-  navigator.bluetooth.requestDevice({filters: [{services: [serviceUUID]}]})
-  .then(device => {
-    bleDevice = device;
-    return device.connectGATT();
-  })
-  .then(server => {
-    bleServer = server;
-    return server.getPrimaryService(options);
-  })
-  .then(service => {
-
-    bleService = service;
-  }).catch(error => {
-
-  });
-}
 //window.onload = fetchData();
 //window.onload = writeToDoc();

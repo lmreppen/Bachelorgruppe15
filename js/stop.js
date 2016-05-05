d2ea88fbfcf4bb3e00bd5c7f81b2d85ea73dc284
lmreppen <lmreppen@stud.ntnu.no>
2016-05-05 10:14:34 +0200

    adding stop.js, editing some in fetc_data.js

---
+++ js/stop.js
@@ -0,0 +1,33 @@
+const serviceUUID = '00001523-1212-efde-1523-785feabcd123';
+
+
+var bleDevice;
+var bleServer;
+var bleService;
+
+
+window.onload = function() {
+    document.querySelector('#stoppKnapp1').addEventListener('click', connect);
+    document.querySelector('#stoppKnapp').addEventListener('click', connect);
+}
+function connect() {
+  if (!navigator.bluetooth) {
+
+      return;
+  }
+  navigator.bluetooth.requestDevice({filters: [{services: [serviceUUID]}]})
+  .then(device => {
+    bleDevice = device;
+    return device.connectGATT();
+  })
+  .then(server => {
+    bleServer = server;
+    return server.getPrimaryService(serviceUUID);
+  })
+  .then(service => {
+
+    bleService = service;
+  }).catch(error => {
+
+  });
+}

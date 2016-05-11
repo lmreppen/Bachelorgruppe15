7340b93e8c3b82850338b1c4294ef406f28036ee
lmreppen <lmreppen@stud.ntnu.no>
2016-05-11 18:00:33 +0200

    til Lars

---
+++ dev-kitexample/pca10040/pca10040/ser_s132_uart/arm5_no_packs/_build/ser_app_power_system_off.d
@@ -0,0 +1,16 @@
+.\_build\ser_app_power_system_off.o: ..\..\..\..\..\..\components\serialization\application\hal\ser_app_power_system_off.c
+.\_build\ser_app_power_system_off.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.5.0\Device\Include\nrf.h
+.\_build\ser_app_power_system_off.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.5.0\Device\Include\nrf52.h
+.\_build\ser_app_power_system_off.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cm4.h
+.\_build\ser_app_power_system_off.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\_build\ser_app_power_system_off.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cmInstr.h
+.\_build\ser_app_power_system_off.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\cmsis_armcc.h
+.\_build\ser_app_power_system_off.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cmFunc.h
+.\_build\ser_app_power_system_off.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\4.5.0\CMSIS\Include\core_cmSimd.h
+.\_build\ser_app_power_system_off.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.5.0\Device\Include\system_nrf52.h
+.\_build\ser_app_power_system_off.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.5.0\Device\Include\nrf52_bitfields.h
+.\_build\ser_app_power_system_off.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.5.0\Device\Include\nrf51_to_nrf52.h
+.\_build\ser_app_power_system_off.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.5.0\Device\Include\nrf52_name_change.h
+.\_build\ser_app_power_system_off.o: C:\Keil_v5\ARM\PACK\NordicSemiconductor\nRF_DeviceFamilyPack\8.5.0\Device\Include\compiler_abstraction.h
+.\_build\ser_app_power_system_off.o: ..\..\..\..\..\..\components\serialization\application\hal\ser_app_power_system_off.h
+.\_build\ser_app_power_system_off.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
